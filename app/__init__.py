c896bd752d982e698e887f341af2a05592880c36
jochen <942814208@qq.com>
2017-09-18 19:50:59 +0800

    flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/Python
 # -*- coding: utf-8 -*-
 from flask import Flask
+import config
 
 app = Flask(__name__)
+app.config.from_object(config)
+
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -3,6 +3,9 @@
 from app import app
 
 @app.route('/')
-@app.route('/')
 def index():
     return"hello,world"
+
+# @app.route('/article/<id>')          #url传参还没成功，标记一下
+# def article(id):
+#     return u'您请求的参数id为：%s' % id
